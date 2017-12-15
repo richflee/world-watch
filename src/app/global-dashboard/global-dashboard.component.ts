66910aaf8007854b679e0356b9a39fed88401a36
Richard Lee <richard.lee@carsales.com.au>
2017-12-16 09:47:18 +1100

    First component and it's module + routing

---
+++ src/app/global-dashboard/global-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-dashboard',
+  templateUrl: './global-dashboard.component.html',
+  styleUrls: ['./global-dashboard.component.scss']
+})
+export class GlobalDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
